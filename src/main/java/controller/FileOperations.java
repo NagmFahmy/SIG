28b7960115d437fa6f7e8efd4f9b63162caca14c
Nagmfahmy@hotmail.com <Nagmfahmy@hotmail.com>
2022-12-08 07:22:48 +0300

    Setting Direct Files without chooser

--- src/main/java/controller/FileOperations.java
+++ src/main/java/controller/FileOperations.java
@@ -30,55 +30,56 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FileOperations implements ActionListener, ListSelectionListener {
-    private InvoiceFrame frame;
+    private final InvoiceFrame frame;
     private InvoiceDialog invoiceDialog;
     private LineDialog lineDialog;
 
     public FileOperations(InvoiceFrame frame) {
         this.frame = frame;
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-    String actionCommand = e.getActionCommand();
+        String actionCommand = e.getActionCommand();
         System.out.println("Action: " + actionCommand);
         switch (actionCommand) {
-        case "Load File":
-            loadFile();
-            break;
-        case "Save File":
-            saveFile();
-            break;
-        case "Create New Invoice":
-            createNewInvoice();
-            break;
-        case "Delete Invoice":
-            deleteInvoice();
-            break;
-        case "Create New Item":
-            createNewItem();
-            break;
-        case "Delete Item":
-            deleteItem();
-            break;
-        case "createInvoiceCancel":
-            createInvoiceCancel();
-            break;
-        case "createInvoiceOK":
-            createInvoiceOK();
-            break;
-        case "createLineOK":
-            createLineOK();
-            break;
-        case "Save Record":
-            updateRecord();
-            break;
-        case "createLineCancel":
-            createLineCancel();
-            break;
+            case "Load File":
+                loadFile();
+                break;
+            case "Save File":
+                saveFile();
+                break;
+            case "Create New Invoice":
+                createNewInvoice();
+                break;
+            case "Delete Invoice":
+                deleteInvoice();
+                break;
+            case "Create New Item":
+                createNewItem();
+                break;
+            case "Delete Item":
+                deleteItem();
+                break;
+            case "createInvoiceCancel":
+                createInvoiceCancel();
+                break;
+            case "createInvoiceOK":
+                createInvoiceOK();
+                break;
+            case "createLineOK":
+                createLineOK();
+                break;
+            case "Save Record":
+                updateRecord();
+                break;
+            case "createLineCancel":
+                createLineCancel();
+                break;
 
         }
 
-}
+    }
 
     @Override
     public void valueChanged(ListSelectionEvent e) {
@@ -97,70 +98,61 @@ public class FileOperations implements ActionListener, ListSelectionListener {
     }
 
     private void loadFile() {
-        JFileChooser fc = new JFileChooser();
+
         try {
-            int result = fc.showOpenDialog(frame);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                File headerFile = fc.getSelectedFile();
-                Path headerPath = Paths.get(headerFile.getAbsolutePath());
-                List<String> headerLines = Files.readAllLines(headerPath);
-                System.out.println("Invoices have been read");
-
-                ArrayList<InvoiceHeader> invoicesArray = new ArrayList<>();
-                for (String headerLine : headerLines) {
-                    try {
-                        String[] headerParts = headerLine.split(",");
-                        int invoiceNum = Integer.parseInt(headerParts[0]);
-                        String invoiceDate = headerParts[1];
-                        String customerName = headerParts[2];
-
-                        InvoiceHeader invoice = new InvoiceHeader(invoiceNum, invoiceDate, customerName);
-                        invoicesArray.add(invoice);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                        JOptionPane.showMessageDialog(frame, "Error in line format", "Error", JOptionPane.ERROR_MESSAGE);
-                    }
+            URL headerResource = InvoiceFrame.class.getClassLoader().getResource("InvoiceHeader.csv");
+            File headerFile = new File(headerResource.toURI());
+            Path headerPath = Paths.get(headerFile.getAbsolutePath());
+            List<String> headerLines = Files.readAllLines(headerPath);
+            System.out.println("Invoices have been read");
+            ArrayList<InvoiceHeader> invoicesArray = new ArrayList<>();
+            for (String headerLine : headerLines) {
+                try {
+                    String[] headerParts = headerLine.split(",");
+                    System.out.println(headerParts[0]);
+                    int invoiceNum = Integer.parseInt(headerParts[0]);
+                    String invoiceDate = headerParts[1];
+                    String customerName = headerParts[2];
+                    InvoiceHeader invoice = new InvoiceHeader(invoiceNum, invoiceDate, customerName);
+                    invoicesArray.add(invoice);
+                } catch (Exception ex) {
                 }
-                System.out.println("Check point");
-                result = fc.showOpenDialog(frame);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File lineFile = fc.getSelectedFile();
-                    Path linePath = Paths.get(lineFile.getAbsolutePath());
-                    List<String> lineLines = Files.readAllLines(linePath);
-                    System.out.println("Lines have been read");
-                    for (String lineLine : lineLines) {
-                        try {
-                            String lineParts[] = lineLine.split(",");
-                            int invoiceNum = Integer.parseInt(lineParts[0]);
-                            String itemName = lineParts[1];
-                            double itemPrice = Double.parseDouble(lineParts[2]);
-                            int count = Integer.parseInt(lineParts[3]);
-                            InvoiceHeader inv = null;
-                            for (InvoiceHeader invoice : invoicesArray) {
-                                if (invoice.getInvoiceNum() == invoiceNum) {
-                                    inv = invoice;
-                                    break;
-                                }
-                            }
+            }
+            System.out.println("Check point");
 
-                            InvoiceLine line = new InvoiceLine(itemName, itemPrice, count, inv);
-                            inv.getLines().add(line);
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                            JOptionPane.showMessageDialog(frame, "Error in line format", "Error", JOptionPane.ERROR_MESSAGE);
+            URL lineResource = InvoiceFrame.class.getClassLoader().getResource("InvoiceLines.csv");
+            File lineFile = new File(lineResource.toURI());
+            Path linePath = Paths.get(lineFile.getAbsolutePath());
+            List<String> lineLines = Files.readAllLines(linePath);
+            System.out.println("Lines have been read");
+            for (String lineLine : lineLines) {
+                try {
+                    String[] lineParts = lineLine.split(",");
+                    System.out.println(lineParts);
+                    int invoiceNum = Integer.parseInt(lineParts[0]);
+                    String itemName = lineParts[1];
+                    double itemPrice = Double.parseDouble(lineParts[2]);
+                    int count = Integer.parseInt(lineParts[3]);
+                    InvoiceHeader inv = null;
+                    for (InvoiceHeader invoice : invoicesArray) {
+                        if (invoice.getInvoiceNum() == invoiceNum) {
+                            inv = invoice;
+                            break;
                         }
                     }
-                    System.out.println("Check point");
+
+                    InvoiceLine line = new InvoiceLine(itemName, itemPrice, count, inv);
+                    inv.getLines().add(line);
+                } catch (Exception ex) {
                 }
-                frame.setInvoices(invoicesArray);
-                InvoiceHeaderTable invoicesTableModel = new InvoiceHeaderTable(invoicesArray);
-                frame.setInvoicesTableModel(invoicesTableModel);
-                frame.getInvoiceTable().setModel(invoicesTableModel);
-                frame.getInvoicesTableModel().fireTableDataChanged();
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(frame, "Cannot read file", "Error", JOptionPane.ERROR_MESSAGE);
+            System.out.println("Check point");
+            frame.setInvoices(invoicesArray);
+            InvoiceHeaderTable invoicesTableModel = new InvoiceHeaderTable(invoicesArray);
+            frame.setInvoicesTableModel(invoicesTableModel);
+            frame.getInvoiceTable().setModel(invoicesTableModel);
+            frame.getInvoicesTableModel().fireTableDataChanged();
+        } catch (Exception ex) {
         }
     }
 
@@ -181,23 +173,19 @@ public class FileOperations implements ActionListener, ListSelectionListener {
         }
         System.out.println("Check point");
         try {
-            JFileChooser fc = new JFileChooser();
-            int result = fc.showSaveDialog(frame);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                File headerFile = fc.getSelectedFile();
-                FileWriter hfw = new FileWriter(headerFile);
-                hfw.write(headers);
-                hfw.flush();
-                hfw.close();
-                result = fc.showSaveDialog(frame);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File lineFile = fc.getSelectedFile();
-                    FileWriter lfw = new FileWriter(lineFile);
-                    lfw.write(lines);
-                    lfw.flush();
-                    lfw.close();
-                }
-            }
+            URL lineResource = InvoiceFrame.class.getClassLoader().getResource("InvoiceLines.csv");
+            URL headerResource = InvoiceFrame.class.getClassLoader().getResource("InvoiceHeader.csv");
+
+            File headerFile = new File(headerResource.toURI());
+            FileWriter hfw = new FileWriter(headerFile);
+            hfw.write(headers);
+            hfw.flush();
+            hfw.close();
+            File lineFile = new File(lineResource.toURI());
+            FileWriter lfw = new FileWriter(lineFile);
+            lfw.write(lines);
+            lfw.flush();
+            lfw.close();
         } catch (Exception ex) {
 
         }
@@ -213,14 +201,11 @@ public class FileOperations implements ActionListener, ListSelectionListener {
         if (selectedRow != -1) {
             frame.getInvoices().remove(selectedRow);
             frame.getInvoicesTableModel().fireTableDataChanged();
-            frame.getLineTable().setModel(new DefaultTableModel(
-                    new Object [][] {
+            frame.getLineTable().setModel(new DefaultTableModel(new Object[][]{
 
-                    },
-                    new String [] {
+            }, new String[]{
 
-                    }
-            ));
+            }));
             frame.getCustomerNameField().setText("");
             frame.getInvoiceNumField().setText("");
             frame.getInvoiceDateField().setText("");
@@ -306,19 +291,20 @@ public class FileOperations implements ActionListener, ListSelectionListener {
         lineDialog.dispose();
         lineDialog = null;
     }
-    private void updateRecord(){
+
+    private void updateRecord() {
         int selectedIndex = frame.getInvoiceTable().getSelectedRow();
-        System.out.println(" index: "+selectedIndex+ " has been updated successfully" );
+        System.out.println(" index: " + selectedIndex + " has been updated successfully");
         if (selectedIndex != -1) {
             System.out.println("You have selected row: " + selectedIndex);
             InvoiceHeader currentInvoice = frame.getInvoices().get(selectedIndex);
             currentInvoice.setInvoiceDate(frame.getInvoiceDateField().getText());
             currentInvoice.setCustomerName(frame.getCustomerNameField().getText());
 
-           frame.getInvoicesTableModel().setValueAt(frame.getInvoiceDateField().getText(),1,selectedIndex);
-           frame.getInvoicesTableModel().setValueAt(frame.getCustomerNameField().getText(),2,selectedIndex);
-           frame.getInvoicesTableModel().fireTableDataChanged();
-           saveFile();
+            frame.getInvoicesTableModel().setValueAt(frame.getInvoiceDateField().getText(), 1, selectedIndex);
+            frame.getInvoicesTableModel().setValueAt(frame.getCustomerNameField().getText(), 2, selectedIndex);
+            frame.getInvoicesTableModel().fireTableDataChanged();
+            saveFile();
         }
     }
 
